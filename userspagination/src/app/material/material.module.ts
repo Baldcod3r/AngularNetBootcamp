79695a43abffed9e106c59a63a43ec3d3c07d636
Agustin Barrale <103079224+Baldcod3r@users.noreply.github.com>
2023-07-14 19:04:34 -0300

    Add files via upload
    Paginacion Y uso de datos desde api externa
    Creacion de Modulos componentes
    Service
    Angular Material

---
+++ userspagination/src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorIntl, MatPaginatorModule} from '@angular/material/paginator';
+import { MatPaginatorIntlEs } from '../usuarios/MatPaginatorIntlEs';
+
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatPaginatorModule,
+
+  ],
+  exports : [
+    MatToolbarModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  providers : [
+    {provide:MatPaginatorIntl, useClass: MatPaginatorIntlEs}
+  ]
+})
+export class MaterialModule { }
